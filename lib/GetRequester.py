807dfbebc64378a203020ae8c488bdb8f2ef2713
judy-chelangat <judysigilai75@gmail.com>
2023-09-18 21:11:48 +0300

    completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -3,11 +3,17 @@ import json
 
 class GetRequester:
 
+
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response =requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        program_list=[]
+        programs =json.loads(self.get_response_body())
+        for program in programs:
+            program_list.append(program)
+        return program_list
